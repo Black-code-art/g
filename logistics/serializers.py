73f60b141622673d06f89592b3299a48a78fa436
Ugwuanyi Emmanuel Oluebube <emmaunelebube@gmail.com>
2024-11-22 01:10:08 +0100

    Initial commit

---
+++ logistics/serializers.py
@@ -0,0 +1,106 @@
+from rest_framework import serializers
+from .models import *
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import User
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import logout
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User  = get_user_model()
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password', 'role')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()
+        return user
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = '__all__'
+
+class ShipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shipment
+        fields = '__all__'
+
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+
+class DriverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = '__all__'
+
+class RouteOptimizationSerializer(serializers.ModelSerializer):
+    class Meta :
+        model =  RouteOptimization
+        fields = '__all__'
+        
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = User
+        fields = "__all__"
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password', 'role')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        user = authenticate(**attrs)
+        if user:
+            return user
+        raise serializers.ValidationError('Invalid credentials')
+
+class LogoutSerializer(serializers.Serializer):
+    message = serializers.CharField(default="Successfully logged out.")    
+
+class LogoutView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = LogoutSerializer  
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return Response({"message": "Successfully logged out."})    
