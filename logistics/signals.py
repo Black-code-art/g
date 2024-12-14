ff6b3a64dc7f2d295b8a1462c1d443a45e52c2a6
Ugwuanyi Emmanuel Oluebube <emmaunelebube@gmail.com>
2024-12-14 22:49:37 +0100

    god pls

--- logistics/signals.py
+++ logistics/signals.py
@@ -2,7 +2,7 @@ import random
 from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_save
 from django.contrib.auth import get_user_model
-from config import settings
+from logistics_app import settings
 import time 
 from django.utils import  timezone
 from .models import *
